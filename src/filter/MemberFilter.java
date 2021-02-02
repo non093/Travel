328ac8e35a507f381b3801b4a68340c9dfe5c2e2
non093 <72378215+non093@users.noreply.github.com>
2021-02-02 19:19:16 +0900

    MemberFilter

---
+++ src/filter/MemberFilter.java
@@ -0,0 +1,37 @@
+package filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebFilter(urlPatterns = {
+		"/member/my.jsp",
+		"/member/edit.jsp", "/member/editPW.jsp",
+		"/member/upload.jsp", "/board/*"
+})
+public class MemberFilter implements Filter{
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		HttpServletRequest req = (HttpServletRequest)request;
+		HttpServletResponse resp = (HttpServletResponse)response;
+		
+		boolean isLogin = req.getSession().getAttribute("check")!=null;
+		
+		if(isLogin) {
+			chain.doFilter(request, response);
+		}
+		else {
+			resp.sendRedirect(req.getContextPath()+"/member/memberLogin.jsp");
+		}
+	}
+
+}
